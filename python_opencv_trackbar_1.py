b2103ae4f31df1e077546de2f3aab504921c6ff2
Akash Mittal <akashmittal18@yahoo.com>
2020-04-02 07:04:00 +0530

    Used histograms and trackbar

---
+++ python_opencv_trackbar_1.py
@@ -0,0 +1,41 @@
+import numpy as np
+import cv2 as cv
+
+
+def nothing(x):
+    print(x)
+
+
+# create a black image,a window
+img = cv.imread('lean.jpg')
+#cv.namedWindow('image')
+
+cv.createTrackbar('CP', 'image', 10, 400, nothing)
+#cv.createTrackbar('G', 'image', 0, 255, nothing)
+#cv.createTrackbar('R', 'image', 0, 255, nothing)
+
+#switch = '0 : OFF\n 1 : ON'
+switch = 'color/gray'
+cv.createTrackbar(switch, 'image', 0, 1, nothing)
+
+while (1):
+    img = cv.imread('lena.jpg')
+    pos = cv.getTrackbarPos('CP', 'image')
+    font = cv.FONT_HERSHEY_SIMPLEX
+    cv.putText(img, str(pos), (50, 150), font, 4, (0, 0, 255))
+    k = cv.waitKey(1) & 0xFF
+    if k == 27:
+        break
+
+    #b = cv.getTrackbarPos('B', 'image')
+    #g = cv.getTrackbarPos('G', 'image')
+    #r = cv.getTrackbarPos('R', 'image')
+    s = cv.getTrackbarPos(switch, 'image')
+
+    if s == 0:
+        pass
+    else:
+        img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    img = cv.imshow('image', img)
+
+cv.destroyAllWindows()
